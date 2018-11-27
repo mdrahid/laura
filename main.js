37144257f01b66beb5f48acfa8f92b4d3a21f991
Md Rahid <44823994+mdrahid@users.noreply.github.com>
2018-11-27 23:22:46 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+jQuery(function($){
+	
+  var typed = new Typed(".type", {
+    strings: ["Professional web developer", "Web Development with no limits","Welcome to my personal page",],
+    smartBackspace: false,
+    typeSpeed: 0,
+    backSpeed: 0,
+    loop: true,
+    startDelay: 1000,
+    backDelay: 700,
+    showCursor: true,
+    cursorChar: ' |',
+    });
+    
+
+
+	
+})(jQuery);	
